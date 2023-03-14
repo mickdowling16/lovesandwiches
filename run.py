d7409d6924c2e464e86b4d1a080418c7f2de092a
mickdowling16 <michaeldowling16@gmail.com>
2023-03-14 16:30:17 +0000

    add funtion to add stock figures to worksheet

--- run.py
+++ run.py
@@ -84,7 +84,21 @@ def get_last_5_entries_sales():
         columns.append(column[-5:])
     return columns
     
+def calculate_stock_data(data):
+    """
+    Calculate the stock data needed for next market
+    """
+    print('Calculating stock data...\n')
+    new_stock_data = []
 
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+    
 
 def main():
     """
@@ -96,8 +110,10 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
     update_worksheet(new_surplus_data, 'surplus')
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 
 print('Welcome to love sandwiches data automation')
 main()
 
-sales_columns = get_last_5_entries_sales()
